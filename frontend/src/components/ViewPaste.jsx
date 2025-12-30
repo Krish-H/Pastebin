1ed9be66a8776fc3bd90707f92b82df48e324b43
krish <krishhh192502@gmail.com>
2025-12-31 00:07:22 +0530

    deployment api links changed

--- frontend/src/components/ViewPaste.jsx
+++ frontend/src/components/ViewPaste.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const API = "http://localhost:5000";
+const API = process.env.API || "http://localhost:5000";
 
 export default function ViewPaste() {
   const { slug } = useParams();
